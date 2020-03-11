378e2acc00dd04a786237a7159ff17f31db5d7a0
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-03-11 14:16:42 +0700

    2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 
                 <!-- Slider Item -->
                 <div class="owl-item home_slider_item">
-                    <div class="home_slider_background" style="background-image:{{asset("images/home_slider_1.jpg"),true}}"></div>
+                    <div class="home_slider_background" style="background-image:url("{{asset("images/home_slider_1.jpg"),true}}")"></div>
                     <div class="home_slider_content_container">
                         <div class="container">
                             <div class="row">
