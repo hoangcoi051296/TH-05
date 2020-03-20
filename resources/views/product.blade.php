f68b1796c0a849c09cf32cbcd985cbf102f2c5cc
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-03-20 10:27:21 +0700

    1

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -33,7 +33,7 @@
                 <!-- Product Image -->
                 <div class="col-lg-6">
                     <div class="details_image">
-                        <div class="details_image_large"><img src="{{$product->thumnail}}" alt=""><div class="product_extra product_new"><a href=""></a></div></div>
+                        <div class="details_image_large"><img src="{{$product->thumnail}}" alt=""><div class="product_extra"></div></div>
                         <div class="details_image_thumbnails d-flex flex-row align-items-start justify-content-between">
                             <div class="details_image_thumbnail active" data-image="images/details_1.jpg"><img src="{{$product->thumnail}}" alt=""></div>
                             <div class="details_image_thumbnail" data-image="images/details_2.jpg"><img src="{{$product->thumnail}}" alt=""></div>
