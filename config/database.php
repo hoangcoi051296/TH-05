be68deb5b2f874a4f1c7eb886c29f1c283e61e0e
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-03-25 17:47:20 +0700

    1

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' =>env('DB_CONNECTION', 'mysql'),//"pgsql",//
+    'default' =>"pgsql",//env('DB_CONNECTION', 'mysql'),//
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' =>env('DB_HOST', '127.0.0.1'),//"ec2-3-231-46-238.compute-1.amazonaws.com",//
-            'port' =>env('DB_PORT', '5432'),5432,//
-            'database' => env('DB_DATABASE', 'forge'),//"d2jtmdft884egd",//
-            'username' => env('DB_USERNAME', 'forge'),//"lezwydnloobxao" ,//
-            'password' =>env('DB_PASSWORD', ''),//"199241b5fb55f4454075e58b995405435da37b84baeed99f85ace1924082ffcf",//
+            'host' =>"ec2-3-231-46-238.compute-1.amazonaws.com",//env('DB_HOST', '127.0.0.1'),//
+            'port' =>5432,//env('DB_PORT', '5432'),
+            'database' => "d2jtmdft884egd",//env('DB_DATABASE', 'forge'),
+            'username' =>"lezwydnloobxao" ,// env('DB_USERNAME', 'forge'),//
+            'password' =>"199241b5fb55f4454075e58b995405435da37b84baeed99f85ace1924082ffcf",//env('DB_PASSWORD', ''),//
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
