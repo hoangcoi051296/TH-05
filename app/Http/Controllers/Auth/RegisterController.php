0b57fb9f45f172dbd8b34cc3ff84ec643aa2280f
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-03-25 17:16:19 +0700

    1

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -34,7 +34,7 @@ class RegisterController extends Controller
      */
 
 
-    protected $redirectTo = '/';
+    protected $redirectTo = '/mail';
 
     /**
      * Create a new controller instance.
@@ -82,5 +82,6 @@ class RegisterController extends Controller
     public function mailSend(){
 
         Mail::to("thaihoangdo0512@gmail.com")->send(new AccountCreated());
+        return view("home");
     }
 }
