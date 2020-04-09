1be86e5594626c81f4476477d82ba4f3e14dfc28
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-04-09 09:34:54 +0700

    1

--- database/migrations/2020_03_06_082049_create_category.php
+++ database/migrations/2020_03_06_082049_create_category.php
@@ -16,6 +16,7 @@ class CreateCategory extends Migration
         Schema::create('category', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('category_name',191)->unique();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
