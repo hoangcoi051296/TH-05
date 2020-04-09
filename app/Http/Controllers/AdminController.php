de1ce2b0a6649fd757976299c47841be5b3b90ba
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-04-09 11:04:05 +0700

    1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -80,7 +80,7 @@ class AdminController extends Controller
     }
     public function categoryEdit($id){
         $category = Category::find($id);
-        dd($category);
+//        dd($category);
         return view ("admin.category.edit",['category'=>$category]);
     }
     public function categoryUpdate($id,Request $request){
